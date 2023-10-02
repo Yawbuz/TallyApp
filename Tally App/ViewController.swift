503921c1560f14f1088374ce59be4bba048d3788
spoons <ev4n.mus1c@gmail.com>
2023-10-01 20:28:40 -0600

    Initial Commit

---
+++ Tally App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tally App
+//
+//  Created by shark boy on 10/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
